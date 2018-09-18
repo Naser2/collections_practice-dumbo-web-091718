c3e61bd4098914bbf86ffbf5e1d3681dce44e40a
Nasser Sanou <nassersanou23@gmail.com>
2018-09-18 13:06:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  return array.sort
+end 
 
+
+def sort_array_desc(array)
+	  array.sort do | first, last|
+	  last <=> first
+	  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |first, last|
+    first.length <=> last.length
+  end 
+end 
+
+
+def swap_elements(array)
+	  array[1], array[2] = array[2], array[1]
+	  array
+end
+
+def reverse_array(array)
+  return array.reverse
+end 
+
+def kesha_maker(array) 
+  array.each do |element|
+    element[2] = "$"
+  end 
+  return array
+end 
+  
+  
+  def find_a(array) 
+ 
+    array.select do |el|
+      # start_w_a.push(el) if el.start_with("a")
+      el[0] == "a"
+      end 
+      # start_w_a
+  end 
+	
+	def sum_array()
+	  
+	end 
